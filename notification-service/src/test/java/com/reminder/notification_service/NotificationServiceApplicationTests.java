0e6bfe04ff9096b47f4adbe0d046fe2b9b48e98a
datnewbie <thanhdattqc@gmail.com>
2026-04-27 18:51:54 +0700

    feat: implement notification service with Kafka consumer and email/SMS support

---
+++ notification-service/src/test/java/com/reminder/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reminder.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
